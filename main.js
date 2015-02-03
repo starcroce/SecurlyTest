489d0812960d858e2caf11ae3f8d8afa9ebe3c3b
starcroce <starcroce@yahoo.com>
2015-02-03 11:10:59 -0800

    init commit

---
+++ main.js
@@ -0,0 +1,9 @@
+chrome.tabs.create({url: "http://www.securly.com"});
+
+chrome.tabs.create({url: "http://www.amazon.com"});
+
+chrome.tabs.create({url: "https://www.youtube.com/results?search_query=porn"});
+
+chrome.tabs.create({url: "https://www.youtube.com"});
+
+chrome.tabs.create({url: "http://www.maxim.com"});

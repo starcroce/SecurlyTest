9f2367e2ff002a385b57d5c95198219ab3c833af
starcroce <starcroce@yahoo.com>
2015-02-09 10:13:48 -0800

    add features of close all tabs with http or https url

--- popup.js
+++ popup.js
@@ -7,13 +7,26 @@ function openSelectedSites() {
     }
 }
 
+function closeAllTabs() {
+    chrome.tabs.query({}, function(tabs) {
+        for (var i = 0; i < tabs.length; i++) {
+            if (tabs[i].url.indexOf("http://") != -1 ||
+                tabs[i].url.indexOf("https://") != -1) {
+
+                chrome.tabs.remove(tabs[i].id);
+            }
+        }
+    });
+}
+
 function actionSites(selected) {
     var selectedSites = document.getElementsByName("site");
-        for (var i = 0; i < selectedSites.length; i++) {
-            selectedSites[i].checked = selected;
-        }
+    for (var i = 0; i < selectedSites.length; i++) {
+        selectedSites[i].checked = selected;
+    }
 }
  
-document.getElementById('open').addEventListener('click', openSelectedSites);
 document.getElementById('selectAll').addEventListener('click', function() {actionSites(true);});
 document.getElementById('clearAll').addEventListener('click', function() {actionSites(false);});
+document.getElementById('closeAll').addEventListener('click', closeAllTabs);
+document.getElementById('open').addEventListener('click', openSelectedSites);

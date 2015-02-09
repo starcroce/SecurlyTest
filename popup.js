f7858ae3aaede7b1c04b89d37de57d1c501fda2d
starcroce <starcroce@yahoo.com>
2015-02-08 17:05:56 -0800

    add feature of select all and clear all sites

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-function hello() {
+function openSelectedSites() {
     var selectedSites = document.getElementsByName("site");
     for (var i = 0; i < selectedSites.length; i++) {
         if (selectedSites[i].checked) {
@@ -6,5 +6,14 @@ function hello() {
         }
     }
 }
+
+function actionSites(selected) {
+    var selectedSites = document.getElementsByName("site");
+        for (var i = 0; i < selectedSites.length; i++) {
+            selectedSites[i].checked = selected;
+        }
+}
  
-document.getElementById('clickme').addEventListener('click', hello);
+document.getElementById('open').addEventListener('click', openSelectedSites);
+document.getElementById('selectAll').addEventListener('click', function() {actionSites(true);});
+document.getElementById('clearAll').addEventListener('click', function() {actionSites(false);});
